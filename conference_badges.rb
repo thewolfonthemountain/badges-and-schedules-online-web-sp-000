4588b1be2d12dff245cd123e39cc08ae8f4c1519
Sean Langton <slangton1@gmail.com>
2018-10-27 02:18:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+def batch_badge_creator(names)
+  names.each {|name| puts "Hello, my name is #{name}"}
+end
+
+def assign_rooms(names)
+  names.each_with_index {|name, index| puts "Hello, #{name}! You'll be assigned to room #{index}+1"}
