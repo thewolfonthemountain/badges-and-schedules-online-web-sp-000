68a051cb3a0576ae1fa22591e4aeb9c2da285261
Sean Langton <slangton1@gmail.com>
2018-10-27 02:23:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,12 @@
 # Write your code here.
-def badge_maker(names)
-  names.each {|name| return "Hello, my name is #{name}"}
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each {|name| badge_maker(name) << badges}
+  return badges
 end
 
 def assign_rooms(names)
